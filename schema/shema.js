740f26cd44f690af11c4b48eaae0ad23254d9f2e
노경모 <kyeongmo2@gmail.com>
2018-05-17 21:53:29 +0900

    Apply GraphQL schema to express-graphql

--- schema/shema.js
+++ schema/shema.js
@@ -1,9 +1,17 @@
 const graphql = require('graphql')
+const _ = require('lodash')
+
 const {
   GraphQLObjectType,
   GraphQLString,
-  GraphQLInt
+  GraphQLInt,
+  GraphQLSchema // this takes in root query and returns GraphQL instance
 } = graphql
+
+const users = [
+  { id: '23', firstName: 'Bill', age: 20 },
+  { id: '47', firstName: 'Samantha', age: 21 },
+]
 
 const UserType = new GraphQLObjectType({
   name: 'User',
@@ -12,4 +20,25 @@ const UserType = new GraphQLObjectType({
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt }
   }
+})
+
+// RootQuery: allows GraphQL enter into database and grab data
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } }, // query requires id argument to find an user
+      
+      // important: this resolve function is the one that actually returns data
+      // second argument args: id above is present on this argument
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id }) // this id will be provided to the query
+      }
+    }
+  }
+})
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
 })
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const expressGraphQL = require('express-graphql')
+const schema = require('./schema/shema')
 
 const app = express()
 
 app.use('/graphql', expressGraphQL({
+  schema,
   graphiql: true // for development purpose
 }))
 
