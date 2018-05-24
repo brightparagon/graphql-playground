6feefcf0a279b91b3cf20b129e8269ff3618b881
노경모 <kyeongmo2@gmail.com>
2018-05-24 22:57:47 +0900

    Add delete song mutation and wire it up to the song list component

--- second-graphql-app/client/components/SongList.js
+++ second-graphql-app/client/components/SongList.js
@@ -29,4 +29,15 @@ class SongList extends Component {
   }
 }
 
-export default graphql(query)(SongList);
+const mutation = gql`
+  mutation DeleteSong($id: ID) {
+    deleteSong(id: $id) {
+      id
+      title
+    }
+  }
+`;
+
+export default graphql(mutation)(
+  graphql(query)(SongList)
+);
