780f3e75ca4d57b2f812a14f922b48d32f6a80eb
노경모 <kyeongmo2@gmail.com>
2018-05-28 00:18:07 +0900

    Update fetchSong query to get lyrics & add renderLyrics method to LyricList component

--- second-graphql-app/client/components/LyricList.js
+++ second-graphql-app/client/components/LyricList.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class LyricList extends Component {
+  renderLyrics() {
+    return this.props.lyrics.map(({ id, content }) => {
+      return (
+        <li key={id} className='collection-item'>
+          {content}
+        </li>
+      );
+    });
+  }
+
   render() {
     return (
-      <ul>
-        lyric list
+      <ul className='collection'>
+        {this.renderLyrics()}
       </ul>
     );
   }
