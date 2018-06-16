d7c76600f91bf963ebd8307c2c86a2b8dd115daa
노경모 <kyeongmo2@gmail.com>
2018-06-16 19:56:35 +0900

    Set mongolab uri in server.js

--- third-graphql-app/server/server.js
+++ third-graphql-app/server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = process.env.MLAB_MONGODB_URI_AUTH;
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
