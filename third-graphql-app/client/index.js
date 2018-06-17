0e80bb0cb5848e449b226ce79ab6e02913902133
노경모 <kyeongmo2@gmail.com>
2018-06-17 18:27:34 +0900

    Add network interface option to Apollo client

--- third-graphql-app/client/index.js
+++ third-graphql-app/client/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
-import ApolloClient from 'apollo-client';
+import ApolloClient, { createNetworkInterface } from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import App from './components/App';
 
+const networkInterface = createNetworkInterface({
+  uri: '/graphql',
+  opts: {
+    credentials: 'same-origin' // this option attatches cookies to requests
+  }
+});
+
+// apollo client is in charge of making requests between react and backend.
+// but GraqhQL doesn't attach cookies to any requests.
+// so it needs a custom option for it.
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject: o => o.id
 });
 
