7e5d3de68800c6091139d028e7c0d1ce7addcfeb
노경모 <kyeongmo2@gmail.com>
2018-06-20 00:31:09 +0900

    Add Dashboard component

--- third-graphql-app/client/components/App.js
+++ third-graphql-app/client/components/App.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import LoginForm from './LoginForm';
 import SignupForm from './SignupForm';
+import Dashboard from './Dashboard';
 
 const App = (props) => {
   return (
@@ -10,7 +11,8 @@ const App = (props) => {
       <Header />
       <Switch>
         <Route exact path='/login' component={LoginForm} />
-        <Route exact path='/signup' component={SignupForm} /> 
+        <Route exact path='/signup' component={SignupForm} />
+        <Route exact path='/dashboard' component={Dashboard} />
       </Switch>
     </div>
   );
