5dc3099d2d0a5680cc803a4b065e7ea59d7c8cbf
노경모 <kyeongmo2@gmail.com>
2018-06-19 23:05:37 +0900

    Add SignupForm component

--- third-graphql-app/client/components/App.js
+++ third-graphql-app/client/components/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
 import LoginForm from './LoginForm';
+import SignupForm from './SignupForm';
 
 const App = (props) => {
   return (
     <div className='container'>
       <Header />
       <Switch>
-        <Route exact path='/login' component={LoginForm} />    
+        <Route exact path='/login' component={LoginForm} />
+        <Route exact path='/signup' component={SignupForm} /> 
       </Switch>
     </div>
   );
